ca454ca0c739ab9a2eaa27e80e241705d4b5e95f
abhishek36 <akamble9913@gmail.com>
2021-04-16 09:19:36 +0530

    first commit

---
+++ src/Component/Title.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title() {
+    return (
+        <div className="title">
+            <h1>firegram</h1>
+            <h2>Your Pictures</h2>
+            <p>You can further verify that the database is operational by connecting. </p>
+        </div>
+    )
+}
+
+export default Title
