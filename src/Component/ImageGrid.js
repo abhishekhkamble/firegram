9abf9f708ca0bdac924449af11113a46ecbed338
Adarsh M <Adarsh.M@resmed.com.au>
2023-03-11 22:11:19 +0530

    image api added

--- src/Component/ImageGrid.js
+++ src/Component/ImageGrid.js
@@ -1,28 +1,51 @@
-import React from 'react'
-import useFirestore from '../Hooks/useFirestore'
-import {motion} from 'framer-motion'
+import React, { useEffect, useState } from "react";
+// import useFirestore from "../Hooks/useFirestore";
+import { motion } from "framer-motion";
 
-function ImageGrid({setSelectedImg}) {
+function ImageGrid({ setSelectedImg }) {
+  // const {docs} = useFirestore('images')
+  const [docs, setDocs] = useState([]);
+  const getImages = async () => {
+    try {
+      const data = await fetch("http://localhost:9090/api/v1/image", {
+        method: "GET",
+      });
 
-    const {docs} = useFirestore('images')
-    console.log(docs)
-    return (
-        <div className="img-grid">
-          {docs && docs.map(doc =>(
-              <motion.div className="img-wrap" key={doc.id}
-              layout
-              whileHover={{opacity : 1}}
-              onClick={()=>setSelectedImg(doc.url)}
-              >
-                  <motion.img src={doc.url} alt='uploaded picture'
-                   initial = {{opacity:0}}
-                   animate ={{opacity:1}}
-                   transition = {{delay:1}}
-                  />
-              </motion.div>
-          ))}
-        </div>
-    )
+      const response = await data.json();
+      setDocs(response.result);
+      console.log(response.result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getImages();
+  }, [setSelectedImg]);
+  console.log(docs);
+  //   console.log(docs);
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <motion.div
+            className="img-wrap"
+            key={doc._id}
+            layout
+            whileHover={{ opacity: 1 }}
+            onClick={() => setSelectedImg(doc.url)}
+          >
+            <motion.img
+              src={doc.url}
+              alt="uploaded picture"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1 }}
+            />
+          </motion.div>
+        ))}
+    </div>
+  );
 }
 
-export default ImageGrid
+export default ImageGrid;
--- src/Component/UploadForm.js
+++ src/Component/UploadForm.js
@@ -1,38 +1,58 @@
-import React, { useState } from 'react'
-import ProgressBar from './ProgressBar';
+import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 function UploadForm() {
-const [file , setFile] = useState(null)
-const [error, setError] = useState(null)
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
 
-const changeHandler = (event) =>{
-    let selected  = event.target.files[0];
+  const changeHandler = (event) => {
+    let selected = event.target.files[0];
+    console.log(selected.name);
 
-    const types = ['image/png' , 'image/jpeg']
+    const types = ["image/png", "image/jpeg"];
 
-    if(selected && types.includes(selected.type)){
-        setFile(selected);
-        setError('')
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+      setError("");
+
+      // upload api
+      const formdata = new FormData();
+      formdata.append("", selected, selected.name);
+
+      const requestOptions = {
+        method: "POST",
+        body: formdata,
+        redirect: "follow",
+      };
+
+      fetch("http://localhost:9090/api/v1/upload", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.log("error", error));
+    } else {
+      setFile(null);
+      setError("Please select an image file (png or jpeg)");
     }
-    else{
-        setFile(null);
-        setError("Please select an image file (png or jpeg)")
-    }
+  };
+
+  return (
+    <form>
+      <label>
+        <input
+          className="label"
+          type="file"
+          onChange={changeHandler}
+          placeholder="+"
+        />
+        <span>+</span>
+      </label>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
+        {/* {file && <ProgressBar file={file} setFile={setFile}/>} */}
+      </div>
+    </form>
+  );
 }
 
-    return (
-        <form>
-            <label>
-            <input className="label" type="file" onChange={changeHandler} placeholder="+"/>
-            <span>+</span>
-            </label>
-            <div className="output">
-                {error && <div className="error">{error}</div>}
-                {file && <div>{file.name}</div>}
-                {file && <ProgressBar file={file} setFile={setFile}/>}
-            </div>
-        </form>
-    )
-}
-
-export default UploadForm
+export default UploadForm;
